ae91716cb067d02d49b50a034d649165565a9992
Imre <nemethi@kkszki.hu>
2025-10-28 08:02:33 +0100

    Visszakódolás megvalósítása

--- Betueltolo/Program.cs
+++ Betueltolo/Program.cs
@@ -10,32 +10,81 @@ namespace Betueltolo
     {
         static string kod = "4567";
         static string abc = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyzaábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz";
+        static string simaabc = "zyxwvüűúutsrqponmlkjíihgfeéedcbáazyxwvüűúutsrqponmlkjíihgfeéedcbáa";
+
         static char Eltolva(char betu, int szam)
         {
-         
-            int mostani_index=abc.IndexOf(betu);
-            
-         
-            return abc[mostani_index+szam];
+
+            int mostani_index = abc.IndexOf(betu);
+
+
+            return abc[mostani_index + szam];
         }
+
+        static char Visszatolva(char betu, int szam)
+        {
+
+            int mostani_index = abc.IndexOf(betu);
+            mostani_index += (abc.Length / 2);
+           
+            return abc[mostani_index - szam];
+        }
+
+
         static void Main(string[] args)
         {
-            
-            string szo = "python";
+            Console.WriteLine("adja meg a szót: ");
+            string szo = Console.ReadLine();
             int index = 0;
-            for (int i = 0; i < szo.Length; i++) {
+            string uj = "";
+            for (int i = 0; i < szo.Length; i++)
+            {
+                if (szo[i] == ' ')
+                {
+                    uj += ' ';
+                    continue;
+                }
+                else
+                {
+                    int szam = int.Parse(kod[index].ToString());
 
-                int szam = int.Parse(kod[index].ToString());
-                Console.WriteLine(Eltolva(szo[i],szam));
-             
+                    uj += Eltolva(szo[i], szam);
+                }
                 index++;
                 if (index == kod.Length)
                 {
-                    index=0;
+                    index = 0;
                 }
-           
+
             }
+            Console.WriteLine(uj);
             Console.ReadLine();
+            index = 0;
+
+            for (int i = 0; i < uj.Length; i++)
+            {
+
+                int szam = int.Parse(kod[index].ToString());
+                if (szam > abc.Length)
+                {
+                    szam += (abc.Length / 2);
+                }
+               
+                Console.WriteLine(Visszatolva(uj[i], szam));
+                
+
+                index++;
+                if (index == kod.Length)
+                {
+                    index = 0;
+                }
+
+
+            }
+
+
+
         }
     }
 }
+
